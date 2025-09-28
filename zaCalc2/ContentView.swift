4553b78dcbe633a41d8d31494185f8e351cdceb5
steve holl <smholl@gmail.com>
2025-09-27 22:19:44 -0400

    Initial Commit

---
+++ zaCalc2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zaCalc2
+//
+//  Created by sholl on 9/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zaCalc2/zaCalc2App.swift
@@ -0,0 +1,17 @@
+//
+//  zaCalc2App.swift
+//  zaCalc2
+//
+//  Created by sholl on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct zaCalc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
